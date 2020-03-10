fd4eac7d8b0b55f2772f2c7f7e33a836dd9d1bd3
harrywhit97 <harrywhit97@gmail.com>
2020-03-10 19:43:23 +0000

    Chnages to pressure readings screne

--- BlazorApp/Data/PressureReadingService.cs
+++ BlazorApp/Data/PressureReadingService.cs
@@ -1,32 +1,41 @@
 using Domain.Models;
 using PressureCore.Concrete;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Repositories;
 
 namespace BlazorApp.Data
 {
     public class PressureReadingService
     {
+        PressureReadingRepository Repository;
 
-        public Task<PressureReading[]> GetPressureReadingAsync()
+        public PressureReadingService()
+        {
+            if(Repository is null)
+                Repository = new PressureReadingRepository();
+        }
+
+        public void AddReading()
+        {
+            Repository.Add(GetPressureReadingAsync(1).Result[0]);
+        }
+
+        public IList<PressureReading> GetReadings()
+        {
+            return Repository.GetPressureReadings();
+        }
+
+        public Task<PressureReading[]> GetPressureReadingAsync(int num)
         {
             var rng = new Random();
-
-            var calculator = new PressureCalculator()
-            {
-                BARMax = 16,
-                ArduinoMaxVoltage = 54,
-                MaxVoltage = 4.24M,
-                ArduinoTotalIntervals = 1024
-            };
-
-            var num = 5;
-
+            var calculator = GetCalculator();
             var rawVals = new int[num];
 
             for (int i = 0; i < num; i++)
-                rawVals[i] = rng.Next(100, 255);                           
+                rawVals[i] = rng.Next(100, 255);
 
             return Task.FromResult(rawVals.Select(rawVal => new PressureReading
             {
@@ -35,6 +44,57 @@ namespace BlazorApp.Data
                 PSI = calculator.CalculatePSI(rawVal),
                 TimeStamp = DateTimeOffset.UtcNow
             }).ToArray());
+        }
+
+        IList<PressureReading> GeneratePressureReadings(int numVals)
+        {
+            var calc = GetCalculator();
+            var rng = new Random();
+
+            var readings = new List<PressureReading>();
+
+            for (int i = 0; i < numVals; i++)
+            {
+                var rawValue = rng.Next(100, 255);
+                readings.Add(GeneratePressureReading(rawValue, calc));
+            }
+
+            return readings;
+        }
+
+        PressureReading GeneratePressureReading(int rawValue, PressureCalculator calculator)
+        {
+            return new PressureReading()
+            {
+                RawValue = rawValue,
+                BAR = calculator.CalculateBAR(rawValue),
+                PSI = calculator.CalculatePSI(rawValue),
+                TimeStamp = DateTimeOffset.UtcNow
+            };
+        }
+
+        PressureCalculator GetCalculator()
+        {
+            return new PressureCalculator()
+            {
+                BARMax = 16,
+                ArduinoMaxVoltage = 54,
+                MaxVoltage = 4.24M,
+                ArduinoTotalIntervals = 1024
+            };
+        }
+    }
+
+    public class PressureReadingSingleton
+    {
+        public static PressureReadingService Service;
+
+        public static PressureReadingService GetService()
+        {
+            if (Service is null)
+                Service = new PressureReadingService();
+
+            return Service;
         }
     }
 }
---
+++ Domain/Repositories/PressureReadingRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repositories
+{
+    public class PressureReadingRepository
+    {
+        IList<PressureReading> Repository;
+
+        public PressureReadingRepository()
+        {
+            Repository = new List<PressureReading>();
+        }
+
+        public void Add(PressureReading reading)
+        {
+            Repository.Add(reading);
+        }
+
+        public IList<PressureReading> GetPressureReadings()
+        {
+            return Repository;
+        }
+    }
+}
